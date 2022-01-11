4a85532005fc2f87c23814a7937504099fb14ab8
Peter-32 <peter@impactradius.com>
2022-01-10 23:20:07 -0800

    init

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+const App = function() {
+  return (
+      <div className="ui container comments">
+          
+      </div>
+  );
+}
+
+ReactDOM.render(
+  <App/>,
+  document.getElementById('root')
+);
