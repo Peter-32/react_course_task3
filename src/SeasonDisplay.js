ca63381f3fe3bc796eeeadf753a2109ddd789201
Peter-32 <peter@impactradius.com>
2022-01-11 23:52:26 -0800

    update

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -5,25 +5,33 @@ import ReactDOM from 'react-dom';
 
 class SeasonDisplay extends React.Component {
 
+    constructor(props){
+        super(props);
 
-    constructor() {
-    super()
-    success(position) {
-        this.x = position.coords.latitude;
-    }
-    this.x = 0
-      window.navigator.geolocation.getCurrentPosition(
-          this.success,
-          (position) => console.log("error")
-      );
-    }
+        this.state = { lat: null, error: null };
+        window.navigator.geolocation.getCurrentPosition(
+          (position) => {
+            this.setState({ lat: position.coords.latitude})
+          },
+          (err) => {
+            this.setState({ error: err.message})
+          }
+        );
+      }
 
     render() {
+        let message = null
+        if (this.state.error) {
+            message = "Error: " + this.state.error;
+        } else if (this.state.lat) {
+            message = "Latitude: " + this.state.lat;
+        } else {
+            message = "Loading";
+        }
+
         return (
-            <div>
-                Season Display {this.x}
-            </div>
-        )
+            <div>{message}</div>
+        );
     }
 }
 
